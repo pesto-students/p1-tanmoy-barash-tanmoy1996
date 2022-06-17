9e227331b1f5ae72e4b3f520905a97a3c9bde724
Tanmoy Barash <barash.tanmoy@gmail.com>
2022-06-17 17:22:29 +0530

    toDo using React class

---
+++ Week-9/todo-app/src/AddTask.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './styles/AddTask.css'
+class AddTask extends Component{
+  handleSubmit(e){
+    e.preventDefault();
+    var form =e.target;
+    const {tasks,newTask} = this.props;
+    var task = {
+      title: form.title.value,
+      desc:form.desc.value,
+      done:false,
+      id:tasks.length>0?tasks[tasks.length-1].id+1:1
+    }
+    form.title.value='';
+    form.desc.value='';
+
+    newTask(task);
+  }
+  cancel(){
+    const {hideAddTask} = this.props;
+    hideAddTask();
+  }
+  render(){
+    return (
+        <div>
+          <form className="form" 
+            onSubmit={(e)=>this.handleSubmit(e)}>
+            <fieldset className="textfield">
+              <input 
+                type="text"
+                name="title"
+                placeholder='Task Title'
+                />
+            </fieldset>
+            <fieldset className="textfield">
+              <textarea
+                rows="4"
+                type="text-area"
+                name="desc"
+                placeholder='Task Desc'
+                />
+            </fieldset>
+                <div className="d-flex">
+                <button className="btn-cancel ml-auto"
+                  onClick={()=>this.cancel()}>
+                    Cancel
+                  </button>
+                  <button type="submit" className="btn addTask">
+                    Add
+                  </button>
+                </div>
+          </form>
+        </div>
+    )
+  }
+}
+
+export default AddTask;
